1104f62355afa0306a70c21689c48b9fb745a963
Angel Villalba <develop@villalba006.dev>
2024-02-28 11:25:06 +0100

    added the onboarding feature and a automated check if user already viewed the onboarding

--- football-frontend/App.tsx
+++ football-frontend/App.tsx
@@ -4,23 +4,72 @@ import React, { useState, useEffect } from "react";
 import { SafeAreaProvider } from "react-native-safe-area-context";
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
-import Navigation from "./src/navigation/NavBar";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import NavBar from "./src/navigation/NavBar";
+import ExamplePage from "./src/screens/organisms/ExamplePage";
+import Onboarding from "./src/screens/organisms/OnboardingPage";
+import { ActivityIndicator } from "react-native-paper";
+
 const Stack = createStackNavigator();
 
+// Loading component
+const Loading = () => {
+  // Display an activity indicator while loading
+  return (
+    <View>
+      <ActivityIndicator size="large" />
+    </View>
+  );
+};
+
+/**
+ * Root component of the FC Barcelona app.
+ * Renders the navigation stack and sets up the app's layout.
+ *
+ * @returns The rendered app component.
+ */
 export default function App() {
+  // State variables
+  const [loading, setLoading] = useState(true);
+  const [viewedOnboarding, setViewedOnboarding] = useState(false);
+  const [initialRoute, setInitialRoute] = useState("Onboarding");
+
+  const checkOnboarding = async () => {
+    try {
+      const value = await AsyncStorage.getItem("viewedOnboarding");
+      if (value !== null) {
+        setViewedOnboarding(true);
+        setInitialRoute("Navigation");
+      }
+    } catch (error) {
+      console.error("Error @checkOnboarding: ", error);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    checkOnboarding();
+  }, []);
+
   return (
     <SafeAreaProvider>
       <NavigationContainer>
         <View style={{ flex: 1 }}>
           <StatusBar style="auto" />
-
-          <Stack.Navigator
-            initialRouteName="Example"
-            screenOptions={{ headerShown: false }}
-          >
-            {/* Screens */}
-            <Stack.Screen name="Example" component={Navigation} />
-          </Stack.Navigator>
+          {loading ? (
+            <Loading />
+          ) : (
+            <Stack.Navigator
+              initialRouteName={initialRoute}
+              screenOptions={{ headerShown: false }}
+            >
+              {/* Screens */}
+              <Stack.Screen name="Onboarding" component={Onboarding} />
+              <Stack.Screen name="Navigation" component={NavBar} />
+              <Stack.Screen name="Example" component={ExamplePage} />
+            </Stack.Navigator>
+          )}
         </View>
       </NavigationContainer>
     </SafeAreaProvider>
---
+++ football-frontend/src/data/json/onboardingSlides.ts
@@ -0,0 +1,27 @@
+export default [
+  {
+    id: "1",
+    title: "Title",
+    description: "Description",
+    image: "./fcb.jpeg",
+  },
+  {
+    id: "2",
+    title: "Title",
+    description: "Description",
+    image:
+      "https://media.istockphoto.com/id/1322220448/photo/abstract-digital-futuristic-eye.jpg?s=612x612&w=0&k=20&c=oAMmGJxyTTNW0XcttULhkp5IxfW9ZTaoVdVwI2KwK5s=",
+  },
+  {
+    id: "3",
+    title: "Title",
+    description: "Description",
+    image: "./fcb.jpeg",
+  },
+  {
+    id: "4",
+    title: "Title",
+    description: "Description",
+    image: "./fcb.jpeg",
+  },
+];
