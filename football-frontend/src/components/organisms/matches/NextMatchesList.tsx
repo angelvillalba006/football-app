78286184c9447fef6540865298ba33357e94602d
Angel Villalba <develop@villalba006.dev>
2024-03-12 07:41:08 +0100

    implemented the backend connection for getting the matches by the league

--- football-frontend/src/components/organisms/matches/NextMatchesList.tsx
+++ football-frontend/src/components/organisms/matches/NextMatchesList.tsx
@@ -1,47 +1,26 @@
-import React from "react";
-import { View, StyleSheet } from "react-native";
+import React, { useEffect, useState } from "react";
+import { View, StyleSheet, Text } from "react-native";
 import SectionTitle from "../../atoms/shared/SectionTitle";
 import NextMatchCard from "../../molecules/matches/NextMatchesCard";
+import MatchdayService from "../../../services/matches";
 
 const NextMatchesList = () => {
-  const matches = [
-    {
-      date: "30 OCT",
-      time: "18:00",
-      homeTeam: {
-        name: "Team A",
-        logo: require("../../../../assets/icons/leagues/premierleague.png"),
-      },
-      awayTeam: {
-        name: "Team B",
-        logo: require("../../../../assets/icons/leagues/premierleague.png"),
-      },
-    },
-    {
-      date: "30 OCT",
-      time: "20:00",
-      homeTeam: {
-        name: "Team C",
-        logo: require("../../../../assets/icons/leagues/premierleague.png"),
-      },
-      awayTeam: {
-        name: "Team D",
-        logo: require("../../../../assets/icons/leagues/premierleague.png"),
-      },
-    },
-    {
-      date: "30 OCT",
-      time: "16:00",
-      homeTeam: {
-        name: "Team E",
-        logo: require("../../../../assets/icons/leagues/premierleague.png"),
-      },
-      awayTeam: {
-        name: "Team F",
-        logo: require("../../../../assets/icons/leagues/premierleague.png"),
-      },
-    },
-  ];
+  const [matches, setMatches] = useState([]);
+
+  useEffect(() => {
+    const fetchMatches = async () => {
+      try {
+        const response = await MatchdayService.getByLeague("premierleague");
+
+        const upcomingMatches = response.filter((match) => !match.played);
+        setMatches(upcomingMatches as any[]);
+      } catch (error) {
+        console.error("Fehler beim Laden der Matches: ", error);
+      }
+    };
+
+    fetchMatches();
+  }, []);
 
   return (
     <View style={styles.container}>
@@ -50,15 +29,19 @@ const NextMatchesList = () => {
         showButton={true}
         buttonText="See all"
       />
-      {matches.map((match, index) => (
-        <NextMatchCard
-          key={index}
-          date={match.date}
-          time={match.time}
-          homeTeam={match.homeTeam}
-          awayTeam={match.awayTeam}
-        />
-      ))}
+      {matches.length > 0 ? (
+        matches.map((match, index) => (
+          <NextMatchCard
+            key={index}
+            date={match.date}
+            time={match.time}
+            homeTeam={match.homeTeam}
+            awayTeam={match.awayTeam}
+          />
+        ))
+      ) : (
+        <Text>No upcoming matches found.</Text>
+      )}
     </View>
   );
 };
